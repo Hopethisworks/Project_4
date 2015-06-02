2f94bd8d6870c2863cf685596b159d4e9add5fdf
Hopethisworks <blhutchinson@bellsouth.net>
2015-06-01 19:24:37 -0500

    updated python
    trying to perform server validation

--- learning.py
+++ learning.py
@@ -23,7 +23,6 @@ DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
     return ndb.Key('Guestbook', guestbook_name)
 
-
     #creating an object class, Link, to store each website link entry
 
 class Link(ndb.Model):
@@ -36,7 +35,23 @@ class Link(ndb.Model):
 # return the links that have been stored in the Link datastore.
 
 class MainPage(webapp2.RequestHandler):
-    def get(self):
+    def get_form(self, error=""):
+        self.response.write(template.render(**template_values))
+
+            template_values = {
+            'links': links,
+            'guestbook_name': urllib.quote_plus(guestbook_name),
+            'error': error,
+        }
+       
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(**template_values))
+        # tried to put substitution here, caused page not to load in web app
+        # self.response.write(template.render(**template_values), % {"error": error})
+        # self.response.write(template.render(**template_values) % {"error": error})
+
+
+    def get(self, error=""):
         # The "get" is to 'pull' information from the datastore
         guestbook_name = self.request.get('guestbook_name',
                                           DEFAULT_GUESTBOOK_NAME)
@@ -45,30 +60,25 @@ class MainPage(webapp2.RequestHandler):
         links = links_query.fetch(15)
 
 
-        template_values = {
-            'links': links,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-        }
-       
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
-
-
-class Guestbook(webapp2.RequestHandler):
-    def post(self):
+    def post(self, error=""):
         # The "post" is to store the information in the datastore
         guestbook_name = self.request.get('guestbook_name',
                                           DEFAULT_GUESTBOOK_NAME)
         link = Link(parent=guestbook_key(guestbook_name))
 
-        link.name = self.request.get('name')
-        link.linkurl = self.request.get('linkurl')
-        link.put()
+        user_name = self.request.get('name')
+        user_url = self.request.get('linkurl')
+
+        if len(user_name) < 5 or len(user_url) < 8:
+            self.response.out.write("Please enter a longer description and a valid URL")
+        else:
+            link.name = self.request.get('name')
+            link.linkurl = self.request.get('linkurl')
+            link.put()
 
         query_params = {'guestbook_name': guestbook_name}
         self.redirect('/?' + urllib.urlencode(query_params))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign', Guestbook),
 ], debug=True)
